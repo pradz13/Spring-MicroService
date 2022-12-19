a8f740bdd5498d2eb92f59d68d51af3d383e1d35
Pradipta <pradipta.nag2@gmail.com>
2022-12-19 18:14:19 +0530

    One WebClient invocation of Rest Service as an example

--- movie-catalog-service/src/main/java/com/microservice/tutorial/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/microservice/tutorial/moviecatalogservice/resource/MovieCatalogResource.java
@@ -5,12 +5,11 @@ import com.microservice.tutorial.moviecatalogservice.model.MovieCatalogItem;
 import com.microservice.tutorial.moviecatalogservice.model.Rating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -22,12 +21,23 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @GetMapping("/movies")
     public List<MovieCatalogItem> getMovieCatalog() {
         List<String> movieIds = List.of("1234", "2456", "6789");
         return movieIds.stream().map(movieId -> {
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + movieId, Movie.class);
-            Rating rating = restTemplate.getForObject("http://localhost:8083/rating-data/" + movieId, Rating.class);
+
+            Rating rating = webClientBuilder
+                    .build()
+                    .get()
+                    .uri("http://localhost:8083/rating-data/" +  movieId)
+                    .retrieve()
+                    .bodyToMono(Rating.class)
+                    .block();
+
             return new MovieCatalogItem(Objects.requireNonNull(movie).getName(), "Desc", Objects.requireNonNull(rating).getRating());
         }).collect(Collectors.toList());
 
