9c9d31f114f0e7e4e009e2a7a2ca5bf0945574c3
Pradipta <pradipta.nag2@gmail.com>
2022-12-19 16:54:45 +0530

    Spring RestTemplat being used as a Singleton Bean

--- movie-catalog-service/src/main/java/com/microservice/tutorial/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/microservice/tutorial/moviecatalogservice/resource/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.microservice.tutorial.moviecatalogservice.resource;
 import com.microservice.tutorial.moviecatalogservice.model.Movie;
 import com.microservice.tutorial.moviecatalogservice.model.MovieCatalogItem;
 import com.microservice.tutorial.moviecatalogservice.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,11 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/movies")
     public List<MovieCatalogItem> getMovieCatalog() {
-        RestTemplate restTemplate = new RestTemplate();
         List<String> movieIds = List.of("1234", "2456", "6789");
         return movieIds.stream().map(movieId -> {
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + movieId, Movie.class);
